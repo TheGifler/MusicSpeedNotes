183a8219ed719e0b18ba3798b68162c3f7ea76c9
TheGifler <65778658+TheGifler@users.noreply.github.com>
2022-03-27 21:42:08 +0100

    New Results Page

---
+++ results.js
@@ -0,0 +1,43 @@
+const rSemibreveResults = document.getElementById(`semibrevecorrect`);
+const rMinumResults = document.getElementById(`minumcorrect`);
+const rCrotchetResults = document.getElementById(`crotchetcorrect`);
+const rQuaverResults = document.getElementById(`quavercorrect`);
+const rTiedQuaversResults = document.getElementById(`tiedquavercorrect`);
+const rSemiquaverResults = document.getElementById(`semiquavercorrect`);
+
+const rTotalResults = document.getElementById(`totalresults`);
+
+
+
+
+
+const displayResults = () =>{
+    let totalAttempts = 0;
+    let totalCorrect = 0;
+    const checkArray = (notename) =>{
+        for (let i = 0; i < MNVArray.length;i++){
+            if (MNVArray[i].name == `${notename}` && MNVArray[i].attempts > 0){
+                document.getElementById(`${notename.toLowerCase().replace(/\s+/g, '')}result`).style.display = `block`;
+                let reference = eval("r" + notename.replace(/\s+/g, '') + "Results"); //changes the reference into the element variable
+               console.log(reference);
+               let referencePencentage = ((100*(MNVArray[i].correct / MNVArray[i].attempts)).toFixed(2))
+                reference.innerHTML = `You answered correctly ${MNVArray[i].correct} out of the ${MNVArray[i].attempts} times. <br> ${referencePencentage}%`
+                totalAttempts += MNVArray[i].attempts;
+                totalCorrect += MNVArray[i].correct;
+
+            }
+        } 
+    }
+    checkArray(`Semibreve`);
+    checkArray(`Minum`);
+    checkArray(`Crotchet`);
+    checkArray(`Quaver`);
+    checkArray(`Tied Quavers`);
+    checkArray(`Semiquaver`);
+    let percentage = (100*(totalCorrect / totalAttempts));
+    rTotalResults.innerHTML = `In total you managed to get ${percentage.toFixed(2)}% `
+
+    
+    
+
+}
